6335cc8aefdb19b95bf81e7987ecb3df4c6ae9a6
Lorenzo Billi <lorebilli@outlook.it>
2017-06-16 15:28:48 +0200

    Added main window to client module
    Client window displays a temporary jpg image as background. Graphic background is done via BackgroundPanel class located inside "shared" module (since Java JPanels cannot natively have images as backgrounds).

--- client/src/Client.java
+++ client/src/Client.java
@@ -3,9 +3,11 @@
  */
 public class Client {
 
+    private static MainWindow clientWindow;
     private static Thread clientConnectionThread;
 
     public static void main(String[] args) {
+        clientWindow = new MainWindow("Spaghetti Western");
         clientConnectionThread = new Thread(new ClientConnectionManager());
         clientConnectionThread.start();
     }
---
+++ shared/src/BackgroundPanel.java
@@ -0,0 +1,22 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * BackgroundPanel class
+ */
+public class BackgroundPanel extends JPanel {
+
+    private Image panelBackground;
+
+    public BackgroundPanel(String backgroundImage) throws IOException {
+        panelBackground = ImageIO.read(new File(backgroundImage));
+    }
+
+    public void paintComponent(Graphics graph) {
+        super.paintComponent(graph);
+        graph.drawImage(panelBackground, 0, 0, this);
+    }
+}
