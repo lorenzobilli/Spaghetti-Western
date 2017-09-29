ecbe5689c2188e32c09667b361dd545ed4d7fd74
Lorenzo Billi <lorebilli@outlook.it>
2017-09-29 14:54:33 +0200

    Added weight inside Path class
    Even thought we might not implement weights inside the game, sceneries can now handle weighted paths.

--- shared/src/Path.java
+++ shared/src/Path.java
@@ -1,7 +1,31 @@
 import org.jgrapht.graph.DefaultWeightedEdge;
 
+import java.security.InvalidParameterException;
+
 /**
  * Path class
  */
 public class Path extends DefaultWeightedEdge {
+
+    private int cost;
+
+    public Path() {
+        setCost(0);
+    }
+
+    public Path(int cost) {
+        setCost(cost);
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        if (cost < 0) {
+            throw new InvalidParameterException("Path weight cannot be less than zero");
+        }
+        this.cost = cost;
+    }
+
 }
