14fe15e50d8b6156fc4132e5317357b464d281f8
Lorenzo Billi <lorebilli@outlook.it>
2017-10-10 04:16:50 +0200

    Missing place added to medium scenery
    MediumScenery and MediumMap modified accordingly. medium_scenery.jpg has been corrected graphically.

--- client/src/MediumMap.java
+++ client/src/MediumMap.java
@@ -1,31 +1,56 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * MediumMap class
  */
 public class MediumMap extends Map {
 
+	private final Dimension buttonDimension = new Dimension(45, 45);
+	private final int borderSize = 15;
+
+	private JButton santaFeButton;
+	private JButton sanRafaelButton;
+	private JButton valverdeButton;
+	private JButton watermillButton;
+	private JButton desertButton;
+	private JButton canyonDiabloButton;
+	private JButton phoenixButton;
+	private JButton tucsonButton;
+	private JButton elPasoButton;
+	private JButton unionPrisonButton;
+
 	public MediumMap() {
 
-		JButton santaFeButton = new JButton();
-		JButton sanRafaelButton = new JButton();
-		JButton valverdeButton = new JButton();
-		JButton watermillButton = new JButton();
-		JButton desertButton = new JButton();
-		JButton canyonDiabloButton = new JButton();
-		JButton phoenixButton = new JButton();
-		JButton tucsonButton = new JButton();
-		JButton elPasoButton = new JButton();
-
+		santaFeButton = new JButton();
 		santaFeButton.addActionListener(e -> sendPlayerMove("Santa Fe"));
+
+		sanRafaelButton = new JButton();
 		sanRafaelButton.addActionListener(e -> sendPlayerMove("San Rafael"));
+
+		valverdeButton = new JButton();
 		valverdeButton.addActionListener(e -> sendPlayerMove("Valverde"));
+
+		watermillButton = new JButton();
 		watermillButton.addActionListener(e -> sendPlayerMove("Watermill"));
+
+		desertButton = new JButton();
 		desertButton.addActionListener(e -> sendPlayerMove("Desert"));
+
+		canyonDiabloButton = new JButton();
 		canyonDiabloButton.addActionListener(e -> sendPlayerMove("Canyon Diablo"));
+
+		phoenixButton = new JButton();
 		phoenixButton.addActionListener(e -> sendPlayerMove("Phoenix"));
+
+		tucsonButton = new JButton();
 		tucsonButton.addActionListener(e -> sendPlayerMove("Tucson"));
+
+		elPasoButton = new JButton();
 		elPasoButton.addActionListener(e -> sendPlayerMove("El Paso"));
+
+		unionPrisonButton = new JButton();
+		unionPrisonButton.addActionListener(e -> sendPlayerMove("Union prison"));
 
 	}
 
