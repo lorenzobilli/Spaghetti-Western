b4b08a11bcec36d53450149eaefcf7f858c97564
Lorenzo Billi <lorebilli@outlook.it>
2018-01-27 17:06:37 +0100

    Dice and ClashManager classes documented
    Some refinements made as well.

--- shared/src/ClashManager.java
+++ shared/src/ClashManager.java
@@ -3,48 +3,96 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
- * ClashManager class
+ * Manages all clash-related routines, such as clash creation, deciding winners and losers, and getting clash results.
  */
 public class ClashManager {
 
+	/**
+	 * Enumeration used to determine which team is the winner, either attackers or defenders.
+	 */
 	public enum Winners {
 		ATTACK,
 		DEFENSE,
 	}
 
+	/**
+	 * Dice of type attack used for attacking.
+	 */
 	private Dice attackDice = new Dice(Dice.Type.ATTACK);
-	private Dice defenseDice = new Dice(Dice.Type.DEFENSE);
-	private List<Integer> attackResult;
-	private List<Integer> defenseResult;
 
+	/**
+	 * Dice of type defense used for defending.
+	 */
+	private Dice defenseDice = new Dice(Dice.Type.DEFENSE);
+
+	/**
+	 * Stores attack results.
+	 */
+	private List<Integer> attackResult = new ArrayList<>();
+
+	/**
+	 * Stores defense results.
+	 */
+	private List<Integer> defenseResult = new ArrayList<>();
+
+	/**
+	 * Creates a new clash and determines the winners by throwing the correct number of dice.
+	 * @param attackers List of attacking players.
+	 * @param defenders List of defending players.
+	 * @return The winners, using a Winners enumeration.
+	 */
 	public Winners clash(List<Player> attackers, List<Player> defenders) {
-		attackResult = new ArrayList<>();
-		defenseResult = new ArrayList<>();
 
 		int attackDiceNumber = attackers.size();
 		int defenseDiceNumber = defenders.size();
 
+		// Throw a dice for each attacker
 		while (attackDiceNumber != 0) {
 			attackDice.throwDice();
-			attackResult.add(attackDice.getValue());
+			try {
+				attackResult.add(attackDice.getValue());
+			} catch (Exception e) {
+				e.getMessage();
+			}
 			attackDiceNumber--;
 		}
+
+		// Throw a dice for each defender
 		while (defenseDiceNumber != 0) {
 			defenseDice.throwDice();
-			defenseResult.add(defenseDice.getValue());
+			try {
+				defenseResult.add(defenseDice.getValue());
+			} catch (Exception e) {
+				e.getCause();
+			}
 			defenseDiceNumber--;
 		}
-
-		attackResult.sort((o1, o2) -> Integer.compare(o2, o1));
-		defenseResult.sort((o1, o2) -> Integer.compare(o2, o1));
 
 		return getWinners();
 	}
 
+	/**
+	 * Internal method which determines who is the winning team of the clash.
+	 * Points are given in the same manner as in the Risiko game:
+	 *  - Dice are sorted in descending order.
+	 *  - Each attack dice is compared in order to the corresponding defense dice, the higher dice gets the point.
+	 *  - Defense has priority over attack, so if an attack dice has the same value of a defense dice, the point is
+	 *    given to the defense.
+	 *  - If there is a mismatch in attack and defense dice numbers, the missing dice number is automatically given to
+	 *    the player with the higher number of dice (i.e. three defense dice vs. two attack dice: a base point is
+	 *    assigned to defense).
+	 * @return A Winners enumeration which contains the winning team.
+	 */
 	private Winners getWinners() {
+
 		int attackersPoints = 0;
 		int defendersPoints = 0;
 
+		// Sort results for dice matching in descending order
+		attackResult.sort((o1, o2) -> Integer.compare(o2, o1));
+		defenseResult.sort((o1, o2) -> Integer.compare(o2, o1));
+
+		// Same number of dice: no base points
 		if (attackResult.size() == defenseResult.size()) {
 			for (Integer attack : attackResult) {
 				for (Integer defence : defenseResult) {
@@ -57,6 +105,7 @@ public class ClashManager {
 			}
 		}
 
+		// More attack dice: assign base points to attackers, then compute remaining points.
 		if (attackResult.size() > defenseResult.size()) {
 			attackersPoints = attackResult.size() - defenseResult.size();
 			for (int index = 0; index < defenseResult.size(); index++) {
@@ -68,6 +117,7 @@ public class ClashManager {
 			}
 		}
 
+		// More defense dice: assign base points to defenders, then compute remaining points.
 		if (defenseResult.size() > attackResult.size()) {
 			defendersPoints = defenseResult.size() - attackResult.size();
 			for (int index = 0; index < attackResult.size(); index++) {
@@ -82,6 +132,10 @@ public class ClashManager {
 		return defendersPoints >= attackersPoints ? Winners.DEFENSE : Winners.ATTACK;
 	}
 
+	/**
+	 * Gets the attack results.
+	 * @return A list of integers containing all attack results.
+	 */
 	public List<Integer> getAttackResult() {
 		if (attackResult == null) {
 			throw new NoSuchElementException("Clash has not been made yet");
@@ -89,6 +143,10 @@ public class ClashManager {
 		return attackResult;
 	}
 
+	/**
+	 * Gets the defense results.
+	 * @return A list of integers containing all defense results.
+	 */
 	public List<Integer> getDefenseResult() {
 		if (defenseResult == null) {
 			throw new NoSuchElementException("Clash has not been made yet");
--- shared/src/Dice.java
+++ shared/src/Dice.java
@@ -1,31 +1,69 @@
 /**
- * Dice class
+ * Models the dice object. A dice can either be an attack dice or a defense dice. All dice have six faces.
  */
 public class Dice {
 
-	public final int FACES_NUMBER = 6;
+	/**
+	 * Determines the number of faces. This is a standard six faces cubic dice.
+	 */
+	private final int FACES_NUMBER = 6;
 
+	/**
+	 * Stores the type of the dice, either attack or defense.
+	 */
 	public enum Type {
 		ATTACK,
 		DEFENSE
 	}
 
-	private int value;
+	/**
+	 * Stores the type of the dice
+	 */
 	private Type type;
 
+	/**
+	 * Stores the value of the dice
+	 */
+	private int value;
+
+	/**
+	 * Checks if the dice has already been thrown or not.
+	 */
+	private boolean thrown;
+
+	/**
+	 * Creates a new dice of a specific type.
+	 * @param type Type of the dice
+	 */
 	public Dice(Type type) {
 		this.type = type;
 	}
 
-	public int getValue() {
+	/**
+	 * Gets the value of the dice.
+	 * @return Value of the dice.
+	 */
+	public int getValue() throws Exception {
+		if (!thrown) {
+			throw new Exception("Dice has not been thrown yet.");
+		}
+		thrown = false;
 		return value;
 	}
 
+	/**
+	 * Gets the type of the dice.
+	 * @return Type of the dice.
+	 */
 	public Type getType() {
 		return type;
 	}
 
+	/**
+	 * Throws the dice.
+	 */
 	public void throwDice() {
 		value = Randomizer.getRandomInteger(FACES_NUMBER);
+		thrown = true;
 	}
 }
